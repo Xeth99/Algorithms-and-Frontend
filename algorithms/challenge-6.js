7dbba3a50580bf4ab6a70404540212111f1fe15b
CXZ3th Dev <emmanuelfemi646@gmail.com>
2023-10-14 09:20:54 +0100

    challenge 6

--- algorithms/challenge-6.js
+++ algorithms/challenge-6.js
@@ -1,3 +1,20 @@
-function fizzBuzz(num) {}
+function fizzBuzz(num) {
+    if (num % 3 == 0){
+        return "fizz";
+      }
+      else if(num % 5 == 0){
+        return "buzz";
+      }
+      else if (num % 3 == 5 && num % 5 == 3){
+        return "fizzbuzz";
+      }
+      else if(num /! 3 || num /! 5){
+        return num;
+      }
+      else{
+        return "false";  
+      }
+ }
+ return fizzBuzz()
 
-module.exports = fizzBuzz;
+// module.exports = fizzBuzz();
